79366678753a96db742d5ea3c915eaa4f77afb4b
BuildTools <skywarspanel@gmail.com>
2024-02-22 16:14:03 +0100

    Reflex Base

--- src/main/kotlin/me/lished/lavarun/Lavarun.kt
+++ src/main/kotlin/me/lished/lavarun/Lavarun.kt
@@ -1,13 +1,18 @@
 package me.lished.lavarun
 
+import me.lished.lavarun.commands.LavarunCommand
+import me.lished.lavarun.game.Game
+import me.lished.lavarun.game.GameStatus
+import org.bukkit.Bukkit
+import org.bukkit.Location
 import org.bukkit.plugin.java.JavaPlugin
 
-class Lavarun : JavaPlugin() {
-    override fun onEnable() {
-        // Plugin startup logic
-    }
+open class Lavarun : JavaPlugin() {
+    var games: ArrayList<Game> = ArrayList()
 
-    override fun onDisable() {
-        // Plugin shutdown logic
+    override fun onEnable() {
+        games.add(Game("Test", null, GameStatus.WAITING, Location(Bukkit.getWorld("e"), 0.0, 90.0, 0.0)))
+
+        getCommand("lavarun")!!.setExecutor(LavarunCommand())
     }
 }
--- src/main/kotlin/me/lished/lavarun/game/Game.kt
+++ src/main/kotlin/me/lished/lavarun/game/Game.kt
@@ -1,3 +1,20 @@
 package me.lished.lavarun.game
 
-data class Game()
+import org.bukkit.Location
+import org.bukkit.World
+import org.bukkit.entity.Player
+
+data class Game(
+    val name: String,
+    var players: List<Player>?,
+    var status: GameStatus = GameStatus.WAITING,
+//    val world: World,
+    val spawn: Location
+)
+
+enum class GameStatus {
+    OFF,
+    WAITING,
+    STARTING,
+    PLAYING
+}
